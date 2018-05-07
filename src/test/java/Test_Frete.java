15f955907ad3a5cb937f1a52ffdc651a1dd58f95
carolinamaciel1 <carolinamaciel37@gmail.com>
2018-05-07 10:45:42 -0300

    segundaversao_trabalhon2

--- src/test/java/Test_Frete.java
+++ src/test/java/Test_Frete.java
@@ -8,20 +8,18 @@ public class Test_Frete extends TestCase{
         @Test
         public void test_CalcularFreteFracionado(){
             NotaFiscal n1 = new NotaFiscal();
-            CalcularFrete calcularFretef = new CalcularFrete();
-            CalculoFrete fretea = new FreteFracionado();
+            CalculoFrete frete1 = new FreteFracionado();
             double resultado_esperado_01=100.00;
-            double resultado_obtido_01= fretea.calcular("6000","6000",n1);
+            double resultado_obtido_01= frete1.calcular("","",n1);
             assertEquals(resultado_esperado_01,resultado_obtido_01);
         }
 
         @Test
         public void test_CalcularFreteLotacao(){
             NotaFiscal n1 = new NotaFiscal();
-            CalcularFrete calcularFretel = new CalcularFrete();
-            CalculoFrete freteb = new FreteLotacao();
+            CalculoFrete frete2 = new FreteLotacao();
             double resultado_esperado_02=250.00;
-            double resultado_obtido_02= freteb.calcular("6000","6000",n1);
+            double resultado_obtido_02= frete2.calcular("","",n1);
             assertEquals(resultado_esperado_02,resultado_obtido_02);
         }
 
@@ -31,7 +29,6 @@ public class Test_Frete extends TestCase{
     @Test
         public void test_MockCalcularFreteFracionado(){
             NotaFiscal n1 = new NotaFiscal();
-            CalcularFrete calcularFretel = new CalcularFrete();
             CalculoFrete fretemock1 = mock(FreteFracionado.class);
             when(fretemock1.calcular("","",n1)).thenReturn(100.00);
             assertEquals(100.00,fretemock1.calcular("","",n1));
@@ -39,7 +36,6 @@ public class Test_Frete extends TestCase{
 
         public void test_MockCalcularFreteLotacao(){
             NotaFiscal n1 = new NotaFiscal();
-            CalcularFrete calcularFrete2 = new CalcularFrete();
             CalculoFrete fretemock2 = mock(FreteLotacao.class);
             when(fretemock2.calcular("","",n1)).thenReturn(250.00);
             assertEquals(250.00,fretemock2.calcular("","",n1));
